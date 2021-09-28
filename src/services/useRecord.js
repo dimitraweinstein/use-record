d0a5e09d6f7cb58ea2a8807f4346b8901a0e3688
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-27 17:05:26 -0700

    passing test

--- src/services/useRecord.js
+++ src/services/useRecord.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export const useRecord = (initialColor) => {
   const [current, setCurrent] = useState(initialColor);
